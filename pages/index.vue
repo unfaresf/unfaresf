8d260c89b9ef39dfc801970e1c1e7270ee2cb3a6
Andy McCoy <Andykmcc@users.noreply.github.com>
2025-01-29 16:08:22 +0000

    attempt to fix modal position (#50)

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,22 @@
 <template>
-  <UContainer>
-    <UCard class="mt-10">
-      <template #header>
-        <h2 class="text-xl mb-4">Report Sighting</h2>
-        <p>Use this form to report fare inspectors on San Francisco Bay Area public transit.</p>
-      </template>
-      <UForm class="flex flex-col gap-2 max-w-prose" ref="form" @submit="onSubmit" :state="formState" :schema="reportSchema">
-        <SelectRoute @on-change="(newRoute:RouteRequest) => formState.route = newRoute" />
-        <SelectStop :route-id="formState.route?.routeId" @on-change="(newStop:StopPost) => formState.stop = newStop" />
-        <UFormGroup label="Inspectors onboard" name="passenger" help="Enable if inspectors are currently onboard.">
-          <UToggle v-model="formState.passenger" />
-        </UFormGroup>
-      </UForm>
-      <template #footer>
-        <div class="flex">
-          <UButton type="submit" label="Report Sighting" class="ml-auto" @click="submitReport"  :disabled="submitting"/>
-        </div>
-      </template>
-    </UCard>
-  </UContainer>
+  <UCard class="mt-10">
+    <template #header>
+      <h2 class="text-xl mb-4">Report Sighting</h2>
+      <p>Use this form to report fare inspectors on San Francisco Bay Area public transit.</p>
+    </template>
+    <UForm class="flex flex-col gap-2 max-w-prose" ref="form" @submit="onSubmit" :state="formState" :schema="reportSchema">
+      <SelectRoute @on-change="(newRoute:RouteRequest) => formState.route = newRoute" />
+      <SelectStop :route-id="formState.route?.routeId" @on-change="(newStop:StopPost) => formState.stop = newStop" />
+      <UFormGroup label="Inspectors onboard" name="passenger" help="Enable if inspectors are currently onboard.">
+        <UToggle v-model="formState.passenger" />
+      </UFormGroup>
+    </UForm>
+    <template #footer>
+      <div class="flex">
+        <UButton type="submit" label="Report Sighting" class="ml-auto" @click="submitReport"  :disabled="submitting"/>
+      </div>
+    </template>
+  </UCard>
 </template>
 
 <script lang="ts" setup>
