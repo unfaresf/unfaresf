31fa99d0f260354c57e56c55336aaffece980f81
Andy McCoy <andy.k.mccoy@gmail.com>
2025-02-08 16:36:08 +0000

    creating new broadcast handles conflict

--- server/api/broadcast.post.ts
+++ server/api/broadcast.post.ts
@@ -1,25 +1,33 @@
 import { DB as db } from "../sqlite-service";
-import { broadcasts as broadcastsTable } from "../../db/schema";
+import { eq } from 'drizzle-orm';
+import { broadcasts as broadcastsTable, reports as reportsTable } from "../../db/schema";
 import { z } from "zod";
 import { createBroadcasts } from "../../shared/utils/abilities";
 
 const broadcastPostBodySchema = z.object({
-  message: z.string().min(8).max(400).trim()
+  message: z.string().min(8).max(400).trim(),
+  reportId: z.number({coerce: true}).int().positive(),
 });
 
 export default defineEventHandler(async (event) => {
   // @ts-ignore TODO https://github.com/nuxt/nuxt/issues/29263
   await authorize(event, createBroadcasts);
 
-  const { message } = await readValidatedBody(event, broadcastPostBodySchema.parse);
+  const { message, reportId } = await readValidatedBody(event, broadcastPostBodySchema.parse);
 
   try {
-    await db.insert(broadcastsTable).values({message}).returning();
+    await db.transaction(async (tx) => {
+      return Promise.all([
+        tx.insert(broadcastsTable).values({message, reportId}).returning(),
+        tx.update(reportsTable).set({reviewedAt: new Date()}).where(eq(reportsTable.id, reportId)),
+      ]);
+    });
+
     setResponseStatus(event, 201);
-  } catch (e: any) {
+  } catch (err:any) {
     throw createError({
       statusCode: 500,
-      statusMessage: e.message,
+      statusMessage: err.message,
     });
   }
 });
