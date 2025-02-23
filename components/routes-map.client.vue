e1eddbf91ed1b331e54e97897bb927d5e58d3aae
Andy McCoy <andy.k.mccoy@gmail.com>
2025-02-23 19:25:02 +0000

    add labels to map

--- components/routes-map.client.vue
+++ components/routes-map.client.vue
@@ -1,30 +1,12 @@
 <template>
   <MglMap
-    :map-style="style"
+    :mapStyle="style"
     :zoom="zoom"
     :center="center"
     :height="mapHeight"
   >
     <MglGeolocateControl />
-
-    <MglVectorSource
-      source-id="trips"
-      :url="tileServerDomain+'/data/trips.json'"
-      :tiles="tripsSourceTiles"
-    >
-      <MglLineLayer
-        layer-id="hot-trips"
-        source-layer="trips"
-        :paint="hotPaint"
-        :filter="hotTrips"
-      ></MglLineLayer>
-      <MglLineLayer
-        layer-id="transit-trips"
-        source-layer="trips"
-        :paint="paint"
-        :filter="tripFilter"
-      ></MglLineLayer>
-    </MglVectorSource>
+    <MglNavigationControl :showCompass="false" />
 
     <MglVectorSource
       source-id="stops"
@@ -43,22 +25,48 @@
         source-layer="stops"
         :paint="stopsLayerCirclesPaint"
         :filter="stopFilter"
-        :minzoom="10"
+        :minzoom="10",
       />
     </MglVectorSource>
 
-    <MglNavigationControl />
+    <MglVectorSource
+      sourceId="trips"
+      :url="tileServerDomain+'/data/trips.json'"
+      :tiles="tripsSourceTiles"
+      :minzoom="6"
+    >
+      <MglLineLayer
+        layer-id="hot-trips"
+        source-layer="trips"
+        :paint="hotPaint"
+        :filter="hotTrips"
+      />
+      <MglLineLayer
+        layerId="transit-trips"
+        sourceLayer="trips"
+        :paint="paint"
+        :filter="tripFilter"
+      />
+      <MglSymbolLayer
+        layerId="transit-trips-labels"
+        sourceLayer="trips"
+        :minzoom="6"
+        :layout="routeSymbolLayout"
+        :paint="routeSymbolPaint"
+        :filter="routeLabels"
+      />
+    </MglVectorSource>
+
   </MglMap>
 </template>
 
 <script setup lang="ts">
-import { MglMap, useMap, MglNavigationControl, MglVectorSource, MglLineLayer, MglCircleLayer, MglGeolocateControl } from '@indoorequal/vue-maplibre-gl';
-import type { CircleLayerSpecification, LngLatLike } from 'maplibre-gl';
+import { MglMap, useMap, MglNavigationControl, MglVectorSource, MglLineLayer, MglCircleLayer, MglGeolocateControl, MglSymbolLayer } from '@indoorequal/vue-maplibre-gl';
+import type { CircleLayerSpecification, LineLayerSpecification, LngLatLike } from 'maplibre-gl';
 import type { RouteResponse } from "./select/route.vue";
 
 const {public: {tileServerDomain} } = useRuntimeConfig();
-
-const style = 'https://api.maptiler.com/maps/streets/style.json?key=DDypiIJ7OGinseJ5cFio';
+const style = 'https://api.maptiler.com/maps/995d0704-eb12-493a-9e6c-41e320a7b94c/style.json?key=DDypiIJ7OGinseJ5cFio';
 const tripsSourceTiles = [ `${tileServerDomain}/data/trips/{z}/{x}/{y}.pbf` ];
 const stopsSourceTiles = [ `${tileServerDomain}/data/stops/{z}/{x}/{y}.pbf` ];
 const center:LngLatLike = [-122.4404,37.7549];
@@ -72,19 +80,33 @@ const hotPaint = {
   "line-color": "#ff6467"
 }
 const mapPadding = {top: 15, bottom:40, left: 15, right: 15};
+const routeSymbolLayout = {
+  'text-field': ['get', 'route_short_name'],
+  'symbol-placement': 'line-center',
+  'text-rotation-alignment': 'map',
+  'text-letter-spacing': 0.15,
+  'symbol-spacing': 5,
+}
+const routeSymbolPaint = {
+  'text-color': '#e6e6e6',
+  'text-halo-color': '#1a1a1a',
+  'text-halo-width': 1.5,
+  'text-halo-blur': 0.75,
+};
+
 const stopsLayerCirclesPaint = {
-  'circle-radius': 5,
-  'circle-stroke-width': 2,
-  'circle-stroke-color' : '#1b5e20',
-  'circle-color': "rgba(0, 0, 0, 0)"
-} as CircleLayerSpecification['paint'];
-const hotStopsLayerCirclesPaint = {
-  'circle-radius': 5,
-  'circle-stroke-width': 2,
+  'circle-radius': 8,
+  'circle-stroke-width': 3,
   'circle-stroke-color' : '#9f0712',
   'circle-color': "rgba(0, 0, 0, 0)"
 } as CircleLayerSpecification['paint'];
-
+const hotStopsLayerCirclesPaint = {
+  'circle-radius': 8,
+  'circle-stroke-width': 0,
+  'circle-stroke-color' : 'rgba(0, 0, 0, 0)',
+  'circle-color': "#9f0712",
+  'circle-blur': 0.7
+} as CircleLayerSpecification['paint'];
 const props = defineProps<{
   route: RouteResponse|null,
   stopId: string|null,
@@ -98,18 +120,25 @@ const visibleStopIds = ref<string[]>([]);
 const mapHeight = computed(() => {
   return isMobile ? '500px': '362px'
 });
-const tripFilter = computed(()=> {
-  return props.route? ["==","route_id",props.route?.routeId] : ["all", false];
+
+const tripFilter = computed(() => {
+  return props.route ? ["==", "route_id", props.route.routeId] : ["all", false];
 });
-const stopFilter = computed(()=> {
-  return props.stopId ? ["==","stop_id", props.stopId] : ["all", false];
+const stopFilter = computed(():CircleLayerSpecification['filter'] => {
+  return props.stopId ? ["==", "stop_id", props.stopId] : ["all", false];
 });
 
-const hotTrips = computed(()=> {
+const hotTrips = computed(():LineLayerSpecification['filter'] => {
   return props.showBroadcasts ? ["in","route_id", ...visibleRouteIds.value] : ["all", false];
 });
-const hotStops = computed(()=> {
+const hotStops = computed(():CircleLayerSpecification['filter'] => {
   return props.showBroadcasts ? ["in","stop_id", ...visibleStopIds.value] : ["all", false];
+});
+
+const routeLabels = computed(():LineLayerSpecification['filter'] => {
+  const routeIds = [...visibleRouteIds.value, props.route?.routeId].filter(r => !!r);
+
+  return props.showBroadcasts ? ["in","route_id", ...routeIds] : ["all", false];
 });
 
 if (props.showBroadcasts) {
@@ -132,6 +161,13 @@ if (props.showBroadcasts) {
       });
     }
   }, {once: true});
+}
+
+// disable map rotation
+if (transitMap && transitMap.map) {
+  transitMap.map.dragRotate.disable();
+  transitMap.map.keyboard.disable();
+  transitMap.map.touchZoomRotate.disableRotation();
 }
 
 watch(() => props.route, async (newRoute) => {
