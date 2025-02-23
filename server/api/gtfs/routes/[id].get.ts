124613b6757a1af444cd565d6ac908b356d21ce0
Andy McCoy <andy.k.mccoy@gmail.com>
2025-02-23 19:25:02 +0000

    add labels to map

--- server/api/gtfs/routes/[id].get.ts
+++ server/api/gtfs/routes/[id].get.ts
@@ -1,9 +1,11 @@
 import { z } from "zod";
 import { getGtfs } from "../../../../shared/utils/abilities";
 import { getRouteTrips } from "../../../utils/routes-cache";
+import {unescape} from 'node:querystring';
+
 
 const gtfsGetRouteParamsSchema = z.object({
-  id: z.string().trim().max(64)
+  id: z.string().trim().max(64).transform(unescape)
 });
 
 export default defineEventHandler(async (event) => {
@@ -11,7 +13,6 @@ export default defineEventHandler(async (event) => {
   await authorize(event, getGtfs);
 
   const { id } = await getValidatedRouterParams(event, gtfsGetRouteParamsSchema.parse);
-
   try {
     // calculating the bbox is time consuming but doesnt change. cacheing...
     return getRouteTrips(id);
