f22b26588c5d47d7c36f350ea357a6579e270276
Aditya Bhumbla <adit.bhumbla@gmail.com>
2025-04-29 23:09:03 -0700

    Add SelectAgency component

--- pages/report.vue
+++ pages/report.vue
@@ -6,6 +6,7 @@
         <p>Use this form to report fare inspectors on San Francisco Bay Area public transit.</p>
       </template>
       <UForm class="flex flex-col gap-2 max-w-prose" ref="form" @submit="onSubmit" :state="formState" :schema="reportSchema">
+        <SelectAgency />
         <SelectRoute :geo="geoLocation" @on-change="(newRoute:RouteResponse) => formState.route = newRoute" />
         <SelectStop :route-id="formState.route?.routeId" :geo="geoLocation" @on-change="(newStop:StopPostResponse) => formState.stop = newStop" />
         <UFormGroup label="Inspectors onboard" name="passenger" help="Enable if inspectors are currently onboard.">
