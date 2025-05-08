15cfa2f3506bbb0ae00e6775846b04103ac1ab0c
Andy McCoy <Andykmcc@users.noreply.github.com>
2025-05-08 16:54:20 -0700

    maybe now it wont make bad posts (#169)

---
+++ server/utils/fetch-unpublished-broadcasts.ts
@@ -0,0 +1,19 @@
+import { and, gte, or, notLike, isNull } from 'drizzle-orm';
+import { DB as db } from "../sqlite-service";
+import { broadcasts as broadcastsTable } from "../../db/schema";
+import { subMinutes } from 'date-fns';
+
+export default async function fetchUnpublishedBroadcasts(platform:string, ttl=30) {
+  return db
+    .select()
+    .from(broadcastsTable)
+    .where(
+      and(
+        gte(broadcastsTable.createdAt, subMinutes(new Date(), ttl)),
+        or(
+          notLike(broadcastsTable.platforms, `%${platform}%`),
+          isNull(broadcastsTable.platforms)
+        )
+      )
+    );
+}
